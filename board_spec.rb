782e480c1f255eefd5da14b54fb235babd85f4d2
Dawid Pytka <dpytka@gmail.com>
2015-11-10 22:26:50 +0100

    improve

--- board_spec.rb
+++ board_spec.rb
@@ -14,14 +14,16 @@ describe Board do
     ships = [
         Ship.new(:battleship),
         Ship.new(:battleship),
+        Ship.new(:battleship),
+        Ship.new(:battleship),
         Ship.new(:battleship)
     ]
     ships.each { |ship| @board.place_random(ship) }
 
-    ships.flat_map { |ship| ship.cells }.uniq.size.must_equal 15
+    ships.flat_map { |ship| ship.cells }.uniq.size.must_equal ships.map(&:size).inject(:+)
   end
 
-  it "finish status when all ship are hit" do
+  it "is finished when all ship are hit" do
     ships = [
         Ship.new(:battleship),
         Ship.new(:battleship),
@@ -33,18 +35,18 @@ describe Board do
     @board.finished?.must_equal true
   end
 
-  it "miss status when missed" do
+  it "can miss ship" do
     @board.check('B', 1).must_equal :miss
   end
 
-  it "hit status when missed" do
+  it "hit the ship" do
     ship = Ship.new(:battleship)
     @board.place_random(ship)
     x_y = find_cell_cordinates(ship.cells.first)
     @board.check(x_y.first, x_y.last).must_equal :hit
   end
 
-  it "sunk status when missed" do
+  it "sunk sunk the ship" do
     ship = Ship.new(:battleship)
     @board.place_random(ship)
     ship.cells[1..-1].each {|cell| cell.hit!}
